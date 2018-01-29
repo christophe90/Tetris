f0ea02f83738426962e7458a96a23af27b4efd8d
Unknown <klervi29@hotmail.com>
2018-01-29 17:24:25 +0100

    version 2 plateau mouvement

--- html/testMouvementPlateau.js
+++ html/testMouvementPlateau.js
@@ -15,20 +15,52 @@ for (var i=0; i<=hauteur; i++) {
 	}
 }
 
-alert(plateau);
-console.log(plateau);
-
 // Creation d'un premier tetrimino
 var tetrimino1 = new Array();
-var tetriHauteur = 2;
-var tetriLargeur = 3;
+var tetriHauteur = 1;
+var tetriLargeur = 2;
+var absTetri = 0;
 
 tetrimino1 = [[0,1,0],[1,1,1]];
 
-// Placememt du tetrimino en début de descente
+// Placememt du tetrimino dans le plateau en début de descente 
 
 for (var i=0; i<=tetriHauteur; i++) {
 	for (var j=0; j<=tetriLargeur; j++) {
-		
+		plateau[i+absTetri][j] = tetrimino1[i][j];
+	}
+}
+
+alert(plateau);
+console.log(plateau);
+
+// Gestion du clavier (touche down et up)
+
+var clavier = {}
+$(document).keydown(function(e) {
+   clavier[e.keyCode] = true;
+});
+$(document).keyup(function(e) {
+   delete clavier[e.keyCode];
+});
+
+// Descente du tetrimino
+for (var direction in clavier) {
+	if (direction == 40) {
+		// mise à zero de l'ancien emplacement du tetrimino
+		for (var i=0; i<=tetriHauteur; i++) {
+			for (var j=0; j<=tetriLargeur; j++) {
+				plateau[i+absTetri][j] = 0; // à modifier car si autre piece dans le voisinage ça ne fonctionne pas
+			}
+		}
+		absTetri += 1; // modif hauteur max du tetrimino
+		// placement du nouveau tetrimino
+		if (absTetri + tetriHauteur <= tetriHauteur) {
+			for (var i=0; i<=tetriHauteur; i++) {
+				for (var j=0; j<=tetriLargeur; j++) {
+					plateau[i+absTetri][j] = tetrimino1[i][j]; // à modifier car si autre piece dans le voisinage ça ne fonctionne pas
+				}
+			}
+		}
 	}
 }
